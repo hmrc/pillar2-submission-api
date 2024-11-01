6a7c12ae0d93915519999ebde45b9a7f7dd34252
MoDweik <180322631+MoDweik@users.noreply.github.com>
2024-11-01 18:04:33 +0000

    Implement OpenAPI specification generator

---
+++ project/JsonToYaml.scala
@@ -0,0 +1,39 @@
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.dataformat.yaml.{YAMLFactory, YAMLGenerator}
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import sbt.Keys._
+import sbt._
+
+import scala.collection.mutable
+import scala.util.Try
+
+object JsonToYaml {
+  val toYaml = taskKey[Unit]("Generate YAML OpenAPI specification from JSON")
+  def settings: Seq[Setting[_]] = Seq(
+    toYaml := {
+
+      val yamlFactory = new YAMLFactory().configure(YAMLGenerator.Feature.WRITE_DOC_START_MARKER, false)
+      val jsonMapper  = new ObjectMapper().registerModule(DefaultScalaModule)
+      val yamlMapper  = new ObjectMapper(yamlFactory).registerModule(DefaultScalaModule)
+
+      val jsonFile: File = baseDirectory.value / "target/swagger/swagger.json"
+      val yamlFile: File = baseDirectory.value / "target/swagger/application.yaml"
+
+      Try {
+        val jsonString = IO.read(jsonFile)
+        val jsonMap    = jsonMapper.readValue(jsonString, classOf[Map[String, Any]])
+
+        val specification = mutable.LinkedHashMap(
+          "openapi"    -> jsonMap("openapi"),
+          "info"       -> (jsonMap("info").asInstanceOf[Map[String, Any]] + ("version" -> version.value.stripSuffix("-SNAPSHOT"))),
+          "tags"       -> jsonMap("tags"),
+          "paths"      -> jsonMap("paths"),
+          "components" -> jsonMap("components")
+        )
+
+        val yamlString = yamlMapper.writeValueAsString(specification)
+        IO.write(yamlFile, yamlString)
+      }.recover { case ex: Exception => streams.value.log.error(s"Failed to convert JSON to YAML: $ex") }
+    }
+  )
+}
