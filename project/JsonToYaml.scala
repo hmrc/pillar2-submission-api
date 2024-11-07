77035edbbc82c5ca8f7a44fb5d33b5045aaefda7
MoDweik <180322631+MoDweik@users.noreply.github.com>
2024-11-07 14:02:34 +0000

    PIL-1335: Improve formatting of generated OAS

--- project/JsonToYaml.scala
+++ project/JsonToYaml.scala
@@ -1,7 +1,7 @@
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.dataformat.yaml.{YAMLFactory, YAMLGenerator}
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
-import com.iheart.sbtPlaySwagger.SwaggerPlugin.autoImport.swagger
+import com.iheart.sbtPlaySwagger.SwaggerPlugin.autoImport.{swagger, swaggerDomainNameSpaces}
 import sbt.Keys._
 import sbt._
 
@@ -14,9 +14,11 @@ object JsonToYaml {
     routesToYamlSpec := {
       swagger.value
 
-      val yamlFactory = new YAMLFactory().configure(YAMLGenerator.Feature.WRITE_DOC_START_MARKER, false)
-      val jsonMapper  = new ObjectMapper().registerModule(DefaultScalaModule)
-      val yamlMapper  = new ObjectMapper(yamlFactory).registerModule(DefaultScalaModule)
+      val yamlFactory = new YAMLFactory()
+        .configure(YAMLGenerator.Feature.WRITE_DOC_START_MARKER, false)
+        .configure(YAMLGenerator.Feature.MINIMIZE_QUOTES, true)
+      val jsonMapper = new ObjectMapper().registerModule(DefaultScalaModule)
+      val yamlMapper = new ObjectMapper(yamlFactory).registerModule(DefaultScalaModule)
 
       val jsonFile: File = baseDirectory.value / "target/swagger/swagger.json"
       val yamlFile: File = baseDirectory.value / "target/swagger/application.yaml"
@@ -33,8 +35,9 @@ object JsonToYaml {
           "components" -> jsonMap("components")
         )
 
-        val yamlString = yamlMapper.writeValueAsString(specification)
-        IO.write(yamlFile, yamlString)
+        val yamlString     = yamlMapper.writeValueAsString(specification)
+        val formattedYaml = swaggerDomainNameSpaces.value.foldLeft(yamlString)((line, path) => line.replace(s"$path.", ""))
+        IO.write(yamlFile, formattedYaml)
       }.map(_ => jsonFile.delete)
         .recover { case ex: Exception => streams.value.log.error(s"Failed to convert JSON to YAML: $ex") }
     }
