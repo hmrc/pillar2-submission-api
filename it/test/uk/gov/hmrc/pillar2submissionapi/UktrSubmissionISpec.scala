b05bf1c9d14b639f582787c1c26067d4b92f4f17
NC-albur <albur@netcompany.com>
2024-11-13 13:46:39 +0000

    Added additional test to cater for duplicate keys and additional fields in requests

--- it/test/uk/gov/hmrc/pillar2submissionapi/UktrSubmissionISpec.scala
+++ it/test/uk/gov/hmrc/pillar2submissionapi/UktrSubmissionISpec.scala
@@ -88,6 +88,32 @@ class UktrSubmissionISpec extends IntegrationSpecBase {
           }
         }
       }
+
+      "has a valid request body containing duplicates fields" should {
+        val request = FakeRequest(POST, routes.UktrSubmissionController.submitUktr.url)
+          .withJsonBody(validRequestJson_duplicateFields)
+
+        "return a 201 CREATED response" in {
+          val application = applicationBuilder().build()
+          running(application) {
+            val result = route(application, request).value
+            status(result) mustEqual CREATED
+          }
+        }
+      }
+
+      "has a valid request body containing extra fields" should {
+        val request = FakeRequest(POST, routes.UktrSubmissionController.submitUktr.url)
+          .withJsonBody(validRequestJson_extraFields)
+
+        "return a 201 CREATED response" in {
+          val application = applicationBuilder().build()
+          running(application) {
+            val result = route(application, request).value
+            status(result) mustEqual CREATED
+          }
+        }
+      }
     }
 
     "Amend a UKTR submission (PUT)" that {
@@ -146,4 +172,71 @@ object UktrSubmissionISpec {
     Json.parse("""{
         |  "badRequest": ""
         |}""".stripMargin)
+
+  val validRequestJson_duplicateFields: JsValue =
+    Json.parse("""{
+                 |  "accountingPeriodFrom": "2024-08-14",
+                 |  "accountingPeriodTo": "2024-12-14",
+                 |  "obligationMTT": true,
+                 |  "obligationMTT": true,
+                 |  "electionUKGAAP": true,
+                 |  "liabilities": {
+                 |    "electionDTTSingleMember": false,
+                 |    "electionUTPRSingleMember": false,
+                 |    "numberSubGroupDTT": 1,
+                 |    "numberSubGroupUTPR": 1,
+                 |    "totalLiability": 10000.99,
+                 |    "totalLiabilityDTT": 5000.99,
+                 |    "totalLiabilityIIR": 4000,
+                 |    "totalLiabilityUTPR": 10000.99,
+                 |    "totalLiabilityUTPR": 10000.99,
+                 |    "liableEntities": [
+                 |      {
+                 |        "ukChargeableEntityName": "Newco PLC",
+                 |        "idType": "CRN",
+                 |        "idValue": "12345678",
+                 |        "amountOwedDTT": 5000,
+                 |        "amountOwedIIR": 3400,
+                 |        "amountOwedUTPR": 6000.5
+                 |      },
+                 |      {
+                 |        "ukChargeableEntityName": "Newco PLC",
+                 |        "idType": "CRN",
+                 |        "idValue": "12345678",
+                 |        "amountOwedDTT": 5000,
+                 |        "amountOwedIIR": 3400,
+                 |        "amountOwedUTPR": 6000.5
+                 |      }
+                 |    ]
+                 |  }
+                 |}""".stripMargin)
+
+  val validRequestJson_extraFields: JsValue =
+    Json.parse("""{
+                 |  "accountingPeriodFrom": "2024-08-14",
+                 |  "accountingPeriodTo": "2024-12-14",
+                 |  "obligationMTT": true,
+                 |  "electionUKGAAP": true,
+                 |  "extraField": "this should not be here",
+                 |  "liabilities": {
+                 |    "electionDTTSingleMember": false,
+                 |    "electionUTPRSingleMember": false,
+                 |    "numberSubGroupDTT": 1,
+                 |    "numberSubGroupUTPR": 1,
+                 |    "totalLiability": 10000.99,
+                 |    "totalLiabilityDTT": 5000.99,
+                 |    "totalLiabilityIIR": 4000,
+                 |    "totalLiabilityUTPR": 10000.99,
+                 |    "liableEntities": [
+                 |      {
+                 |        "ukChargeableEntityName": "Newco PLC",
+                 |        "idType": "CRN",
+                 |        "idValue": "12345678",
+                 |        "amountOwedDTT": 5000,
+                 |        "amountOwedIIR": 3400,
+                 |        "amountOwedUTPR": 6000.5
+                 |      }
+                 |    ]
+                 |  }
+                 |}""".stripMargin)
 }
