2b34a4d5e2b1929126330592ad959c51bcee7e6c
olakunlekuye2 <100710237+olakunlekuye2@users.noreply.github.com>
2025-04-09 09:43:22 +0100

    Updated validation

--- app/uk/gov/hmrc/pillar2submissionapi/models/overseasreturnnotification/ORNSubmission.scala
+++ app/uk/gov/hmrc/pillar2submissionapi/models/overseasreturnnotification/ORNSubmission.scala
@@ -16,8 +16,7 @@
 
 package uk.gov.hmrc.pillar2submissionapi.models.overseasreturnnotification
 
-import play.api.libs.json.{Json, OFormat}
-
+import play.api.libs.json.{Json, JsonValidationError, OFormat, Reads}
 import java.time.LocalDate
 
 case class ORNSubmission(
@@ -31,5 +30,37 @@ case class ORNSubmission(
 )
 
 object ORNSubmission {
-  implicit val ORNSubmission: OFormat[ORNSubmission] = Json.format[ORNSubmission]
+
+  private val countryGIRReads: Reads[String] =
+    implicitly[Reads[String]].filter(JsonValidationError("countryGIR must be between 1 and 2 characters"))(str => str.length >= 1 && str.length <= 2)
+
+  private val reportingEntityNameReads: Reads[String] =
+    implicitly[Reads[String]].filter(JsonValidationError("reportingEntityName must be between 1 and 200 characters"))(str =>
+      str.length >= 1 && str.length <= 200
+    )
+
+  private val tinReads: Reads[String] =
+    implicitly[Reads[String]].filter(JsonValidationError("TIN must be between 1 and 200 characters"))(str => str.length >= 1 && str.length <= 200)
+
+  private val issuingCountryTINReads: Reads[String] =
+    implicitly[Reads[String]].filter(JsonValidationError("issuingCountryTIN must be between 1 and 2 characters"))(str =>
+      str.length >= 1 && str.length <= 2
+    )
+
+  implicit val reads: Reads[ORNSubmission] = {
+    import play.api.libs.functional.syntax._
+    import play.api.libs.json._
+
+    (
+      (JsPath \ "accountingPeriodFrom").read[LocalDate] and
+        (JsPath \ "accountingPeriodTo").read[LocalDate] and
+        (JsPath \ "filedDateGIR").read[LocalDate] and
+        (JsPath \ "countryGIR").read(countryGIRReads) and
+        (JsPath \ "reportingEntityName").read(reportingEntityNameReads) and
+        (JsPath \ "TIN").read(tinReads) and
+        (JsPath \ "issuingCountryTIN").read(issuingCountryTINReads)
+    )(ORNSubmission.apply _)
+  }
+
+  implicit val format: OFormat[ORNSubmission] = Json.format[ORNSubmission]
 }
