8434b755d812aa298121f581e68d09173e381865
olakunlekuye2 <100710237+olakunlekuye2@users.noreply.github.com>
2025-05-07 20:26:23 +0100

    updated tests

--- test/uk/gov/hmrc/pillar2submissionapi/services/OverseasReturnNotificationServiceSpec.scala
+++ test/uk/gov/hmrc/pillar2submissionapi/services/OverseasReturnNotificationServiceSpec.scala
@@ -141,5 +141,70 @@ class OverseasReturnNotificationServiceSpec extends UnitTestBaseSpec with ORNDat
     }
   }
 
-  // TODO: Add tests for retrieveORN method
+  "retrieveORNService" when {
+    "retrieveORN() called with valid accounting periods" should {
+      "return 200 OK response" in {
+        when(mockOverseasReturnNotificationConnector.retrieveORN(any[String], any[String])(any[HeaderCarrier]))
+          .thenReturn(Future.successful(HttpResponse.apply(200, Json.toJson(retrieveOrnResponse), Map.empty)))
+
+        val result = await(ornService.retrieveORN("2024-01-01", "2024-12-31"))
+
+        assertEquals(retrieveOrnResponse, result)
+      }
+    }
+  }
+
+  "retrieveORN() unexpected 200 response back" should {
+    "Runtime exception thrown" in {
+      when(mockOverseasReturnNotificationConnector.retrieveORN(any[String], any[String])(any[HeaderCarrier]))
+        .thenReturn(Future.successful(HttpResponse.apply(200, Json.toJson("unexpected success response"), Map.empty)))
+
+      intercept[UnexpectedResponse.type](await(ornService.retrieveORN("2024-01-01", "2024-12-31")))
+    }
+  }
+
+  "retrieveORN() 404 response back" should {
+    "ResourceNotFoundException thrown" in {
+      when(mockOverseasReturnNotificationConnector.retrieveORN(any[String], any[String])(any[HeaderCarrier]))
+        .thenReturn(Future.successful(HttpResponse.apply(404, Json.toJson("Not Found"), Map.empty)))
+
+      intercept[ResourceNotFoundException.type](await(ornService.retrieveORN("2024-01-01", "2024-12-31")))
+    }
+  }
+
+  "retrieveORN() 422 response with No Form bundle found" should {
+    "ResourceNotFoundException thrown" in {
+      when(mockOverseasReturnNotificationConnector.retrieveORN(any[String], any[String])(any[HeaderCarrier]))
+        .thenReturn(Future.successful(HttpResponse.apply(422, Json.toJson(ORNErrorResponse("005", "No Form bundle found")), Map.empty)))
+
+      intercept[ResourceNotFoundException.type](await(ornService.retrieveORN("2024-01-01", "2024-12-31")))
+    }
+  }
+
+  "retrieveORN() valid 422 response back" should {
+    "DownstreamValidationError thrown" in {
+      when(mockOverseasReturnNotificationConnector.retrieveORN(any[String], any[String])(any[HeaderCarrier]))
+        .thenReturn(Future.successful(HttpResponse.apply(422, Json.toJson(ORNErrorResponse("093", "Invalid Return")), Map.empty)))
+
+      intercept[DownstreamValidationError](await(ornService.retrieveORN("2024-01-01", "2024-12-31")))
+    }
+  }
+
+  "retrieveORN() unexpected 422 response back" should {
+    "UnexpectedResponse thrown" in {
+      when(mockOverseasReturnNotificationConnector.retrieveORN(any[String], any[String])(any[HeaderCarrier]))
+        .thenReturn(Future.successful(HttpResponse.apply(422, Json.toJson("unexpected error response"), Map.empty)))
+
+      intercept[UnexpectedResponse.type](await(ornService.retrieveORN("2024-01-01", "2024-12-31")))
+    }
+  }
+
+  "retrieveORN() 500 response back" should {
+    "UnexpectedResponse thrown" in {
+      when(mockOverseasReturnNotificationConnector.retrieveORN(any[String], any[String])(any[HeaderCarrier]))
+        .thenReturn(Future.successful(HttpResponse.apply(500, Json.toJson(InternalServerError.toString()), Map.empty)))
+
+      intercept[UnexpectedResponse.type](await(ornService.retrieveORN("2024-01-01", "2024-12-31")))
+    }
+  }
 }
