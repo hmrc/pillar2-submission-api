6f67a4a95bc2eb1657994b856a9eb987f55a1964
Nikolas <gianick@users.noreply.github.com>
2026-04-17 16:50:14 +0100

    Tidied up build.sbt and PR

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,14 +4,14 @@ object AppDependencies {
 
   private val bootstrapVersion = "10.7.0"
 
-  val compile = Seq(
+  val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                  %% "bootstrap-backend-play-30" % bootstrapVersion,
     "com.fasterxml.jackson.module" %% "jackson-module-scala"      % "2.20.0",
     "org.typelevel"                %% "cats-core"                 % "2.13.0",
     "com.beachape"                 %% "enumeratum-play-json"      % "1.9.0"
   )
 
-  val test = Seq(
+  val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-30" % bootstrapVersion % Test,
     "org.scalatest"          %% "scalatest"              % "3.2.19"         % Test,
     "com.vladsch.flexmark"    % "flexmark-all"           % "0.64.8"         % Test,
@@ -23,10 +23,7 @@ object AppDependencies {
   )
 
   val it: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-30" % bootstrapVersion % Test,
-    "com.vladsch.flexmark"    % "flexmark-all"           % "0.64.8"         % Test,
-    "org.mockito"             % "mockito-core"           % "5.20.0"         % Test,
-    "org.scalatestplus"      %% "mockito-4-11"           % "3.2.18.0"       % Test,
-    "org.scalatestplus.play" %% "scalatestplus-play"     % "7.0.2"          % Test
+    "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapVersion % Test
   )
+
 }
